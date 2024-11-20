f6de04de8ddd0e1078d622bb74bf1b3d5b52e642
sahaf-greenwave123 <sahaf.mostafa94@gmail.com>
2024-11-20 06:11:22 +0600

    array

---
+++ array.js
@@ -0,0 +1,133 @@
+// node array.js 
+
+// var student = ['Iamran', 'sadman', ' sahaf', 'mostafa'];
+//console.log(student.length );
+
+
+// var vowel =[ 'a', 'e' ,'i' ,'o' ,'u'  ];
+//console.log(vowel.length);
+
+
+//console.log(mix.length);
+//const mixArrayIndex = mix.indexOf(255);
+//console.log(mixArrayIndex);
+
+//var findFixIndex = mix [55]
+//console.log(findFixIndex);
+
+
+
+
+//how to push sahaf before ali???????
+
+//var mix = ['Ali', 25, false, 'Maria', 'profession', 1239754];
+
+
+//var index = mix.indexOf('Ali');
+
+
+//if (index !== -1) {
+   // mix.splice(index, 0, 'sahaf');
+//}
+
+//console.log(mix); 
+
+
+
+// var mix = ['Ali' ,25 ,false,'Maria' ,'profession' ,1239754]
+//console.log(mix);
+//mix[2] = 'Handsom'
+//mix.push('Arafat')
+//console.log(mix);
+// mix.pop()
+// mix.pop()
+// mix.pop()
+// mix.pop()
+//mix.shift()
+
+//console.log(mix);
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5
+//      == 6);
+// console.log(6 == 6);
+// console.log(6 != 6);
+// console.log(5 != 6);
+
+
+// var mylove = 99;
+// var herlove =100;
+//  console.log(mylove > herlove);
+// console.log(mylove < herlove);
+// console.log(mylove == herlove);
+// console.log(mylove != herlove);
+
+
+
+
+
+
+
+
+// goodstudy && goodperson
+// hasmoney 
+
+
+var iphone = 160000;
+var myBudget = 90000;
+// if (iphone < myBudget) {
+//     console.log('iphone kine futani marbo');
+// }
+
+
+// if (iphone > myBudget) {
+//     console.log('Amar sadh na mitilo');
+// }
+// if (iphone > myBudget) {
+//     console.log('iphone kine futani marbo');
+// }
+
+
+// else  {
+//     console.log('Amar sadh na mitilo');
+// }
+
+
+
+
+
+// if(danis < money){
+//     console.log('danis diye tea khamu');
+// }
+//  else if (batterBan < money){
+//     console.log('batterBan diye tea khamu');
+// }
+//  else if (tost < money){
+//     console.log('tost diye tea khamu');
+// }
+// else{
+//     console.log('batasa diye tea khamu');
+// }
+
+
+// var money = 100;
+// var danis = 150;
+// var batterBan = 110;
+// var tost = 118;
+// var wellPacked = true;
+
+
+
+// if (danis < money){
+//     if(wellPacked == true){
+//         console.log('moja re!!!');
+//     }
+//     else {
+//         console.log('na');
+//     }
+// }
+
+
+var tourDestination = ['cox bazar','nepal','srilanka', 'island']
+tourDestination[0] = 'Rajshahi'
+console.log(tourDestination);
